0c701188ab722c0b1e63434b40eb4fb9fe252db8
Anas Maged <ansmaged5@gmail.com>
2024-01-14 22:29:04 +0200

    Adding attributes that contains the white and black pieces

--- HeaderFiles/game.h
+++ HeaderFiles/game.h
@@ -1,15 +1,22 @@
 #ifndef GAME
 #define GAME
 
-#include "board.h"
+class Board;
+class Piece;
 
 class Game{
     private:
         Board *board;
-
+        vector<Piece*> WhitePieces;
+        vector<Piece*> BlackPieces;
     public:
         Game();
+        
         void play();
+        
+        vector<Piece*> get_white_pieces();
+        
+        vector<Piece*> get_black_pieces();
 };
 
 
--- SrcFiles/game.cpp
+++ SrcFiles/game.cpp
@@ -1,11 +1,43 @@
 #include "..\HeaderFiles\ChessConstans.h"
 #include "..\HeaderFiles\board.h"
-#include "..\HeaderFiles\game.h"
+#include "..\HeaderFiles\game.h" 
+#include "..\HeaderFiles\square.h" 
+#include "..\HeaderFiles\piece.h"
 
 Game::Game(){
     this->board = new Board();
+    for(int i = 0 ; i < 8 ; i++){
+        for(int j = 0 ; j < 8 ; j++){
+            if(board->board[i][j]->getPiece() != nullptr){
+                Piece *p = board->board[i][j]->getPiece();
+                if(p->getColor() == Black){
+                    BlackPieces.emplace_back(p);
+                }
+                else{
+                    WhitePieces.emplace_back(p);
+                }
+            }
+        }
+    }
+}
+
+vector<Piece*> Game::get_black_pieces(){
+    return BlackPieces;
+}
+
+vector<Piece*> Game::get_white_pieces(){
+    return WhitePieces;
 }
 
 void Game::play(){
-    board->display(); 
+    while(true){
+        board->display({}); 
+        cout << "Enter the position of the piece you want to move: ";
+        char row, col; cin >> col >> row;
+        int i = 7 - (row - '1'), j = col - 'a';
+        auto valid_moves = board->board[i][j]->getPiece()->get_valid_moves(board->board, row, col);
+        board->display(valid_moves);
+        Sleep(5000);
+        break;
+    }
 }
