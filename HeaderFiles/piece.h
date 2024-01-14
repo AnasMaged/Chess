8f9fce6d2d7455cff47c618e4ef509fa4a2d08c8
Anas Maged <ansmaged5@gmail.com>
2024-01-14 16:45:58 +0200

    Adding a function to get valid moves for some piece

--- HeaderFiles/piece.h
+++ HeaderFiles/piece.h
@@ -1,27 +1,32 @@
 #ifndef PIECE
 #define PIECE
 
-#include <iostream>
-#include <string.h>
-#include "ChessConstans.h"
-
-using namespace std;
-
+class Board;
 
 class Piece{
     private:
         PieceType type;
         Color color;
         char row, col;
+        vector<int> dx, dy;
+        int max_moves;
     public:
+        Piece();
         Piece(PieceType type, Color color, char row, char col);
-
+        
         PieceType getType();
         
         Color getColor();
 
         void setPosition(char row, char col);
+        
+        void set_max_moves(int max_moves);
 
+        void set_dx(vector<int> dx);
+
+        void set_dy(vector<int> dy);
+        
+        set<pair<char, char>> get_valid_moves(Board* board, char row, char col);
 };
 
 
--- SrcFiles/piece.cpp
+++ SrcFiles/piece.cpp
@@ -1,5 +1,8 @@
-#include "../HeaderFiles/piece.h"
 #include "../HeaderFiles/ChessConstans.h"
+#include "../HeaderFiles/piece.h"
+#include "../HeaderFiles/board.h"
+
+Piece::Piece(){}
 
 Piece::Piece(PieceType type, Color color, char row, char col) 
             : type(type), color(color), row(row), col(col) {}
@@ -18,3 +21,28 @@ void Piece::setPosition(char row, char col){
 }
 
 
+void Piece::set_max_moves(int max_moves){
+    this->max_moves = max_moves;
+}
+
+void Piece::set_dx(vector<int> dx){
+    this->dx = dx;
+}
+
+void Piece::set_dy(vector<int> dy){
+    this->dy = dy;
+}
+
+set<pair<char, char>> Piece::get_valid_moves(Board* board, char row, char col){
+    set<pair<char, char>> valid_moves;
+    int i = row - 'a', j = col - '1';
+    for(int k = 0; k < (int)this->dx.size(); k++){
+        int x = i , y = j;
+        while(valid(x, y)){
+            valid_moves.insert(make_pair((char)(x + 'a'), (char)(y + '1')));
+            x += dx[k];
+            y += dy[k];
+        }
+    }
+    return valid_moves;
+}
