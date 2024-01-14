c31da64d03383616323e2c6b9a26e359d4ce9a37
Anas Maged <ansmaged5@gmail.com>
2024-01-14 22:30:07 +0200

    Fixing the directions of moves

--- SrcFiles/pawn.cpp
+++ SrcFiles/pawn.cpp
@@ -4,8 +4,8 @@
 
 PawnPiece::PawnPiece(PieceType type, Color color, char row, char col) : Piece(type, color, row, col){
     set_max_moves(1);
-    set_dx({0});
-    set_dy({1});
+    set_dx({-1});
+    set_dy({0});
 }
 
 
