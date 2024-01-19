d9b7dec3a95d4d13723bd478086fba8c90a25613
Anas Maged <ansmaged5@gmail.com>
2024-01-19 04:24:38 +0200

    Add addition attribute

--- HeaderFiles/pawn.h
+++ HeaderFiles/pawn.h
@@ -6,9 +6,13 @@
 
 class PawnPiece : public Piece{
     private:
-
+        int first_move;
     public:
         PawnPiece(PieceType type, Color color, char row, char col);
+
+        int get_first_move();
+
+        void set_first_move(int x);
 };
 
 
--- SrcFiles/pawn.cpp
+++ SrcFiles/pawn.cpp
@@ -4,8 +4,23 @@
 
 PawnPiece::PawnPiece(PieceType type, Color color, char row, char col) : Piece(type, color, row, col){
     set_max_moves(1);
-    set_dx({-1});
+    if(color == Black){
+        set_dx({1});
+    }
+    else{
+        set_dx({-1});
+    }
     set_dy({0});
+    first_move = -1;
+}
+
+int PawnPiece::get_first_move(){
+    return this->first_move;
+}
+
+void PawnPiece::set_first_move(int x){
+    this->first_move = x;
+    return;
 }
 
 
